9211b6246822c2be306215d0c3afc0d8f0346931
deebarizo <deebarizo@gmail.com>
2017-11-02 07:53:30 -0500

    Misc

--- client/main.js
+++ client/main.js
@@ -3,4 +3,27 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-console.log('I am the client');
+var img_data = [
+	{
+		img_src: 'red_bird.jpg',
+		img_alt: 'red bird'
+	},
+	{
+		img_src: 'red_green_bird.jpg',
+		img_alt: 'red green bird'
+	},
+	{
+		img_src: 'white_bird.jpg',
+		img_alt: 'white bird'
+	}
+];
+
+Template.images.helpers({ images: img_data });
+
+Template.images.events({
+	'click .js-image': function(event) {
+		$(event.target).css('width', '50px');
+	}
+});
+
+
--- server/main.js
+++ server/main.js
@@ -1,6 +1,31 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  console.log('I am the server');
+	Images = new Mongo.Collection('images');
+
+	console.log(Images.find().count());
+
+	if (Images.find().count() == 0) {
+		Images.insert(
+			{
+				img_src: 'red_bird.jpg',
+				img_alt: 'red bird'
+			}
+		);
+
+		Images.insert(
+			{
+				img_src: 'red_green_bird.jpg',
+				img_alt: 'red green bird'
+			}
+		);
+
+		Images.insert(
+			{
+				img_src: 'white_bird.jpg',
+				img_alt: 'white bird'
+			}
+		);
+	}
 });
 
